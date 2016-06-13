ce7d4c7587f04512e779676f2a0dfa6fb61d38f7
Christian <khrizt@gmail.com>
2016-06-13 14:30:00 +0200

    Trim response header values

--- src/Khrizt/PushNotiphications/Model/BaseResponse.php
+++ src/Khrizt/PushNotiphications/Model/BaseResponse.php
@@ -14,10 +14,10 @@ abstract class BaseResponse
             foreach (explode("\r\n", $rawHeaders) as $key => $header) {
                 if ($key === 0) {
                     // get status from headers
-                    $headers['httpCode'] = str_replace('HTTP/2 ', '', $header);
+                    $headers['httpCode'] = trim(str_replace('HTTP/2 ', '', $header));
                 } elseif (!empty(trim($header))) {
                     list($key, $value) = explode(': ', $header);
-                    $headers[$key] = $value;
+                    $headers[$key] = trim($value);
                 }
             }
         }
